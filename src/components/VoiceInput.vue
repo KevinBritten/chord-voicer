338748cc87a155c4993c0bf98648138787753dbf
Kevan <kevinjbritten@gmail.com>
2020-12-26 14:57:20 -0500

    linked voice inputs to main voice input component

--- src/components/VoiceInput.vue
+++ src/components/VoiceInput.vue
@@ -1,9 +1,22 @@
 <template>
-  <div><input type="number" /></div>
+  <div>
+    <input
+      type="number"
+      @input="$emit('update-voice-value', { voiceValue, voiceNumber })"
+      v-model="voiceValue"
+    />
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  props: ["voice-number"],
+  data() {
+    return {
+      voiceValue: 0
+    };
+  }
+};
 </script>
 
 <style></style>
--- src/components/VoiceInputs.vue
+++ src/components/VoiceInputs.vue
@@ -4,6 +4,7 @@
       v-for="i in voiceQuantity"
       :key="i"
       :voice-number="i"
+      @update-voice-values="updateVoiceValues"
     ></voice-input>
   </div>
 </template>
@@ -13,10 +14,17 @@ import VoiceInput from "./VoiceInput.vue";
 export default {
   data() {
     return {
-      voiceQuantity: 4
+      voiceQuantity: 4,
+      voiceValues: [0, 0, 0, 0]
     };
   },
-  components: { VoiceInput }
+  components: { VoiceInput },
+  methods: {
+    updateVoiceValues(inputData) {
+      let position = inputData.voiceNumber - 1;
+      this.voiceValues[position] = inputData.voiceValue;
+    }
+  }
 };
 </script>
 
